07d1a93f26cd1749d2c4b7b5df8ce8093f7cc8c6
Henry Desktop <henrymzhao@gmail.com>
2017-05-01 21:26:38 -0700

    mostly done with rails tut

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,12 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :sections do
+    member do
+      get :delete
+    end
+  end
+
   get 'demo/index'
   get 'demo/hello'
   get 'demo/other_hello'
